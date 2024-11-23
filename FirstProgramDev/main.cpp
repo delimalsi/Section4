4ee612131fea9ee63590984d0388e3b46e6d22c2
Jose de Lima <jdelima@snimbus.com>
2024-11-22 21:42:32 -0600

    fix project files

---
+++ FirstProgramDev/main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+    
+    int favoriteNumber;
+    std::cout << "Enter your favorite number between 1 and 100: ";
+        
+    std::cin >> favoriteNumber;
+    
+    std::cout << "Amazing!! You must me a 2 year old to still have a favorite number." << std::endl;
+    return 0;
+}
